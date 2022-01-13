ab0af0b703ff87c6ed2fa8a9a1b866162f08036e
Geoff Jamieson <geoff.ijamieson@gmail.com>
2022-01-13 15:37:04 -0500

    Added Edit and Tech Modal Comps

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -18,9 +21,12 @@ const App = () => {
   return (
     <Fragment>
       <SearchBar />
-      <div className="container">
+      <div className='container'>
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const AddLogModal = () => {
+const EditLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
@@ -20,10 +20,9 @@ const AddLogModal = () => {
   };
 
   return (
-    <div id='add-log-modal' className='modal' style={modalStyle}>
+    <div id='edit-log-modal' className='modal' style={modalStyle}>
       <div className='modal-content'>
         <h4>Enter System Log</h4>
-        <br />
         <div className='row'>
           <div className='input-field'>
             <input
@@ -92,4 +91,4 @@ const modalStyle = {
   height: "75%",
 };
 
-export default AddLogModal;
+export default EditLogModal;
